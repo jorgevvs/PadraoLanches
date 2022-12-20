627fb557601ac3d8ef73eda4a98c7d4a70f4ce1c
Jorgevvs <jorgevinicius9@gmail.com>
2022-12-19 22:49:31 -0300

    teste

--- Aplicação/main.java
+++ Aplicação/main.java
@@ -1,30 +1,17 @@
 package Aplicação;
 
 import Aplicação.Builders.PedidoBuilder;
-import Aplicação.Estados.EstadosPedido.Disponivel;
 import Common.Ingrediente;
-import Common.Pedido;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        PedidoBuilder builder = PedidoBuilder.getInstancia();
-        Pedido pedido = builder.comIngrediente(new Ingrediente("", 1.0))
-                .comObservacoes("")
-                .Finaliza();
-
-        pedido.getEstado().Produzir(pedido);
-        System.out.println(pedido.getEstado());
-        pedido.getEstado().Finalizar(pedido);
-        System.out.println(pedido.getEstado());
-        pedido.getEstado().Cancelar(pedido);
-        System.out.println(pedido.getEstado());
-
-        System.out.println(pedido.Valor);
-
+        List<Ingrediente> EstoqueDisponivel = AbastecerEstoque();
+        MenuPrincipal(EstoqueDisponivel);
     }
-    public static void MenuPrincipal(){
+    public static void MenuPrincipal(List<Ingrediente> estoque){
         Scanner in = new Scanner(System.in);
         System.out.println("========== Padrão Lanches ==========");
         System.out.println("Gostaria de realizar um novo pedido?");
@@ -35,13 +22,29 @@ public class main {
         int resposta = Integer.parseInt(in.nextLine());
 
         if(resposta == 1){
-            NovoPedido();
+            NovoPedido(estoque);
         }
     }
 
 
-    public static void NovoPedido(){
 
+    public static void NovoPedido(List<Ingrediente> estoque){
+        PedidoBuilder CriadorDePedido = PedidoBuilder.getInstancia();
     }
 
+    public static List<Ingrediente> AbastecerEstoque() {
+        List<Ingrediente> ingredientes = new ArrayList<>();
+        ingredientes.add(new Ingrediente("Cebola", 0.50));
+        ingredientes.add(new Ingrediente("Picles", 1.00));
+        ingredientes.add(new Ingrediente("Cheddar", 2.00));
+        ingredientes.add(new Ingrediente("Batata Palha", 1.50));
+        ingredientes.add(new Ingrediente("Ovo de Codorna", 2.50));
+        ingredientes.add(new Ingrediente("Calabresa", 2.50));
+        ingredientes.add(new Ingrediente("Catupiry", 2.00));
+        ingredientes.add(new Ingrediente("Presunto", 1.00));
+        ingredientes.add(new Ingrediente("Molhos", 0.50));
+        ingredientes.add(new Ingrediente("Carne", 5.00));
+        ingredientes.add(new Ingrediente("Frango", 4.00));
+        return ingredientes;
+    }
 }
